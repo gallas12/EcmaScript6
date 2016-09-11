8940e0addad0a23eaf7946df1a87fdb5bc02c84c
gallas <gallas@onio.cz>
2016-09-11 11:52:38 +0200

    Create initial object

---
+++ Human.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Milan Gallas on 11.9.2016.
+ */
+class Human {
+
+    constructor(name){
+        this.name = name;
+    }
+
+    toString(){
+        return "Hello Object world "+this.name;
+    }
+}
+
+
+//var me = new Human("Gali");
+//me.toString()
